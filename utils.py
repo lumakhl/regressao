629278e6975f13a9720510ff867b7c6c8baa1b79
lumakhl <kuhl.luma@gmail.comlumakhl>
2019-06-03 21:10:11 -0300

    criando funções parte dois

--- utils.py
+++ utils.py
@@ -40,6 +40,9 @@ def regressao(X,Y):
 
 def coeficienteRegressao(X,Y,i):
         return B0(X,Y) + (B1(X,Y) * X[i])
+
+def regmultipla(y,X):
+        
                 
 x1 = [10,8,13,9,11,14,6,4,12,7,5]
 y1 = [8.04,6.95,7.58,8.81,8.33,9.96,7.24,4.26,10.84,4.82,5.68]
